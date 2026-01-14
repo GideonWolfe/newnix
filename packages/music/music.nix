3c3cfc6aef913cbe5f38612dfabb68c89050ccfc
GideonWolfe <gideon@gideonwolfe.com>
2026-01-14 01:22:01 -0500

    append

--- packages/music/music.nix
+++ packages/music/music.nix
@@ -2,7 +2,9 @@
 
 {
   environment.systemPackages = with pkgs; [
-    # Utilities
+    #############
+    # Utilities #
+    #############
     fmit # instrument tuner
     lingot # instrument tuner
     vmpk # Virtual MIDI Piano Keyboard
@@ -14,6 +16,30 @@
     frescobaldi # Lillypond GUI for writing scores
     kdePackages.minuet # Learn music theory
     solfege # Learn music theory
+
+    ##################
+    # Music Taggers  #
+    ##################
+    pkgs.eartag # GNOME GUI for editing audio metadata
+    pkgs.easytag # GUI for editing audio metadata
+    pkgs.picard # musicbrainz tagger
+
+    ##################
+    # Music Players  #
+    ##################
+    #pkgs.spotify # disabled because stylix/spicetify install the binary
+    pkgs.cmus # TUI music player
+    pkgs.termusic # TUI music player
+    pkgs.termsonic # TUI subsonic client
+    pkgs.fum # MPRIS TUI client
+    #pkgs.sublime-music #GUI Subsonic music server client #BUG: this one slow as fuck
+    #pkgs.feishin # GUI Subsonic music server client #TODO: rewritten into audioling, not a package yet
+    #pkgs.finamp # Jellyfin music player
+    #pkgs.supersonic # subsonic client https://github.com/dweymouth/supersonic/blob/main/res/themes/default.toml
+    #pkgs.amarok # KDE GUI music player
+    #pkgs.deadbeef-with-plugins # GUI music player unmaintained, might need to just use deadbeef
+    pkgs.decibels # GNOME simple audio player GUI
+
   ];
 }
 
