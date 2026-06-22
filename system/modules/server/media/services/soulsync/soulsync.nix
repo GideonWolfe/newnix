2518936ec52ba551c34fd907daa13735a51ea941
GideonWolfe <gideon@gideonwolfe.com>
2026-06-22 19:53:20 -0400

    add mnemosyne to monitoring pool

--- system/modules/server/media/services/soulsync/soulsync.nix
+++ system/modules/server/media/services/soulsync/soulsync.nix
@@ -2,7 +2,7 @@
 {
   virtualisation.oci-containers.containers.soulsync-webui = {
     # https://hub.docker.com/r/boulderbadgedad/soulsync/tags
-    image = "boulderbadgedad/soulsync:2.3";
+    image = "boulderbadgedad/soulsync:2.7.5";
     extraOptions = [ "--network=media" ];
     ports = [
       "${builtins.toString config.custom.world.services.soulsync-webui.port}:8008"
